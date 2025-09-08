601b2257dec88b122b5ca0a245b2816f625cc223
Park-gi-yeol <v2611317@naver.com>
2025-09-08 12:46:27 +0900

    [level 0] Title: 카운트 업, Time: 0.04 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int start_num, int end_num) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = start_num; i <= end_num; i++) {
+			arr.add(i);
+		}
+        
+        return arr;
+    }
+}
