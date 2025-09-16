effc5b70dc347f89ce5bae41ced942b4d322c858
Park-gi-yeol <v2611317@naver.com>
2025-09-16 13:46:04 +0900

    [level 0] Title: 배열 만들기 1, Time: 10.51 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int n, int k) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		for (int i = 1; i <= n; i++) {
+			if(i % k == 0) {
+				answer.add(i);
+			}
+		}
+        
+        
+        return answer;
+    }
+}
