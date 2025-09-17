1f60103cccbea15776cb42d28115bc91ed959f55
Park-gi-yeol <v2611317@naver.com>
2025-09-17 18:18:42 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.06 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double num = Math.sqrt(n);
+        if(num % 1 == 0) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
