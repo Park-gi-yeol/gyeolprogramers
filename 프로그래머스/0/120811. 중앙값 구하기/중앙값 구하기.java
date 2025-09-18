08f497fa4eb43d8132648afb6d9d7267e55a6612
Park-gi-yeol <v2611317@naver.com>
2025-09-18 14:24:21 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.51 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,17 +1,10 @@
+
+import java.util.*;
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        int num= 0;
-        for(int i = 0; i < array.length; i++) {
-            for(int j = 0; j < array.length; j++) {
-                if(array[i] > array[j] && i != j) {
-                    num = array[i];
-                    array[i] = array[j];
-                    array[j] = num;
-                }
-            }
-        }
-        answer = array[(array.length - 1) / 2];
+        Arrays.sort(array);
+        
+        int answer = array[(array.length - 1) / 2];
         return answer;
     }
 }
