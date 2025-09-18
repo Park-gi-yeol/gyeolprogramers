32845c517f339bc3efde37665ef886fe11c313ae
Park-gi-yeol <v2611317@naver.com>
2025-09-18 12:06:42 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,26 +1,11 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] vowels = {"a","e","i","o","u"};  // 모음 배열
-
-        for(int i = 0; i < my_string.length(); i++) {
-            String ch = String.valueOf(my_string.charAt(i));  // 한 글자 추출
-            boolean isVowel = false;
-
-            // 모음인지 확인
-            for(int j = 0; j < vowels.length; j++) {
-                if(ch.equals(vowels[j])) {
-                    isVowel = true;
-                    break;
-                }
-            }
-
-            // 모음이 아니면 answer에 추가
-            if(!isVowel) {
-                answer += ch;
-            }
-        }
-
-        return answer;
+        String[] arr2 = {"a","e","i","o","u"};
+        
+       for(int i = 0; i < arr2.length; i++) {
+    	my_string = my_string.replace(arr2[i], "");
+       }
+        return my_string;
     }
 }
