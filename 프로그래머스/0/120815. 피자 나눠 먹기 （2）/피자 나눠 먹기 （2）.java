475aecf07e8b5e2924dfbf6eb2236f71f1f03976
Park-gi-yeol <v2611317@naver.com>
2025-09-24 09:14:36 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int pizza = 6;
+        int i = 1;
+		while (true) {
+			
+			if(pizza * i % n == 0 && n!= 0) {
+				answer = i;
+				break;
+			}
+			i++;
+		}
+        return answer;
+    }
+}   
