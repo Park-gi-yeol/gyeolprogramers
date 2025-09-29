9da937edbdb769b5a0d150c9fcde08bfb2ddce8b
Park-gi-yeol <v2611317@naver.com>
2025-09-29 20:13:51 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.61 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(String numbers) {
+
+        long answer = 0;
+		String[] number = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+		for(int i = 0; i < number.length; i++) {
+			numbers = numbers.replaceAll(number[i], String.valueOf(i));
+		}
+		
+		answer = Long.valueOf(numbers);
+        return answer;
+    }
+}
