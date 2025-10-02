db72327422f5937459ee4e4ca35408d1de65d78e
Park-gi-yeol <v2611317@naver.com>
2025-10-02 16:46:46 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+
+		int x = board[0] / 2;
+		int y = board[1] / 2;
+		for (int i = 0; i < keyinput.length; i++) {
+			if ((keyinput[i].equals("down") || keyinput[i].equals("up"))) {
+				if (keyinput[i].equals("down") && answer[1] > -y) {
+					answer[1]--;
+				} else if (keyinput[i].equals("up") && answer[1] < y) {
+					answer[1]++;
+				}
+			}
+			if ((keyinput[i].equals("left") || keyinput[i].equals("right"))) {
+				if (keyinput[i].equals("left") && answer[0] > -x) {
+					answer[0]--;
+				} else if(keyinput[i].equals("right") && answer[0] < x){
+					answer[0]++;
+				}
+			}
+			
+		}
+        return answer;
+    }
+}
