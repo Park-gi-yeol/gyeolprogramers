647498725d905bba9f86aaf22bc1202fef712fad
Park-gi-yeol <v2611317@naver.com>
2026-04-16 04:36:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] arr = new int[num_list.length];
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = num_list[i];
-        }
-        
-        // answer 배열: 기존 요소 + 마지막 계산값
-        int[] answer = new int[arr.length + 1];
-        for(int i = 0; i < arr.length; i++) {
-            answer[i] = arr[i];
-        }
-        
-        // 마지막 요소 계산
-        if(arr[arr.length - 1] > arr[arr.length - 2]) {
-            answer[arr.length] = arr[arr.length -1] - arr[arr.length -2];
-        } else {
-            answer[arr.length] = arr[arr.length -1] * 2;
-        }
-        
+        int[] answer = new int[num_list.length+1];
+		int a = 0;
+		if(num_list[num_list.length -1] > num_list[num_list.length-2]) {
+			a = num_list[num_list.length-1] - num_list[num_list.length-2];
+		} else {
+			a = num_list[num_list.length-1] * 2;
+		}
+		for(int i = 0; i < num_list.length; i++) {
+			answer[i] = num_list[i];
+		}
+		answer[answer.length-1] = a;
         return answer;
     }
 }
