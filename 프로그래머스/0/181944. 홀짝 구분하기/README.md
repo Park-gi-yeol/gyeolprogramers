d9f5888cca65a102ea380654b39f9c0bd74fd187
Park-gi-yeol <v2611317@naver.com>
2026-04-16 00:47:59 +0900

    [level 0] Title: 홀짝 구분하기, Time: 186.49 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/README.md
+++ 프로그래머스/0/181944. 홀짝 구분하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀짝 구분하기 - 181944 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181944?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181944) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 171.31 ms
+메모리: 71.5 MB, 시간: 186.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 01일 16:01:28
+2026년 04월 16일 00:47:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,11 +4,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-        if (n % 2 == 0) {
-            System.out.println(n + " is even");
-        } else {
+        if(n % 2 == 1) {
             System.out.println(n + " is odd");
+        } else {
+            System.out.println(n + " is even");
         }
     }
 }
