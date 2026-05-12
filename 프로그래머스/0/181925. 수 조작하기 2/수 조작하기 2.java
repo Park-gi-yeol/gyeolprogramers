85681d62e8d2c6bcc59b537f79871563cc81d9c2
Park-gi-yeol <v2611317@naver.com>
2026-05-12 14:27:40 +0900

    [level 0] Title: 수 조작하기 2, Time: 608.44 ms, Memory: 362 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,20 +1,15 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        int num = 0;
         for(int i = 1; i < numLog.length; i++) {
-        	if(numLog[i] - numLog[i-1] == 1) {
-        		answer += "w";
-        	} else if (numLog[i] - numLog[i-1] == -1 ) {
-        		answer += "s";
-        	} else if (numLog[i] - numLog[i-1] == 10) {
-        		answer += "d";
-        	} else if (numLog[i] - numLog[i-1] == -10){
-        		answer += "a";
-        	} else {
-        		
-        	}
-        }
+			int j = numLog[i] - numLog[i - 1];
+			switch (j) {
+				case 1 : answer += "w"; break;
+				case -1 : answer += "s"; break;
+				case 10 : answer += "d"; break;
+				case -10 : answer += "a"; break;			
+			}
+		}
         return answer;
     }
 }
