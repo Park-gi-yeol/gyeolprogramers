f3e3be7703c806cc430371ff610ef56b0f531e42
Park-gi-yeol <v2611317@naver.com>
2026-05-12 11:23:25 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,20 +1,19 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int a = 0;
+        int a = 0 ;
 		int b = 0;
 		for(int i = 0; i < num_list.length; i++) {
-			if(i % 2 ==0) {
+			if(i % 2 == 0 ) {
 				a += num_list[i];
-			} else {
+			}
+		}
+		for(int i = 0; i < num_list.length; i++) {
+			if(i % 2 == 1 ) {
 				b += num_list[i];
 			}
 		}
-		if( a > b) {
-			answer = a;
-		} else {
-			answer = b;
-		}
+		answer = (a > b) ? a : b;
         return answer;
     }
 }
