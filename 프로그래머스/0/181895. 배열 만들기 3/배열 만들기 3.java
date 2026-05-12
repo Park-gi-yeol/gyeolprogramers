30af8947d5fdad6a0e7eeac820cc218f7c30511b
Park-gi-yeol <v2611317@naver.com>
2026-05-12 12:53:48 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.29 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,26 +1,16 @@
-import java.util.ArrayList;
 class Solution {
-    public ArrayList<Integer> solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-		
-		int[] arr1 = new int[intervals[0][1] - intervals[0][0] + 1];
-		int[] arr2 = new int[intervals[1][1] - intervals[1][0] + 1];
-		int j = 0;
-		for(int i = intervals[0][0]; i < intervals[0][1]+1; i++) {
-			arr1[j] = arr[i];
-			j++;
+    public int[] solution(int[] arr, int[][] intervals) {
+        int idx = 0;
+		int[] answer = new int[(intervals[0][1] - intervals[0][0] + 1) + (intervals[1][1] - intervals[1][0] + 1)];
+		for(int i = intervals[0][0]; i <= intervals[0][1]; i++) {		
+			answer[idx] =  arr[i];
+			idx++;			
 		}
-		j = 0;
-		for(int i = intervals[1][0]; i < intervals[1][1]+1; i++) {
-			arr2[j] = arr[i];
-			j++;
+		for(int i = intervals[1][0]; i <= intervals[1][1]; i++) {
+			answer[idx] = arr[i];
+			idx++;
 		}
-		for(int i = 0; i < arr1.length; i++) {
-			answer.add(arr1[i]);
-		}
-		for(int i = 0; i < arr2.length; i++) {
-			answer.add(arr2[i]);
-		}
+
         return answer;
     }
 }
