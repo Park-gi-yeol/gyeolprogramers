ad10e42c23f09e6da4e27cbf286158456b224217
Park-gi-yeol <v2611317@naver.com>
2026-05-12 10:33:29 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 3.47 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        myString = myString.toLowerCase();
-		pat = pat.toLowerCase();
-		if(myString.contains(pat)) {
+        if(myString.toLowerCase().contains(pat.toLowerCase())) {
 			answer = 1;
-		}
+		} 
         return answer;
     }
 }
