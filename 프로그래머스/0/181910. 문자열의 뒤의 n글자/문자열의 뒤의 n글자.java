20f2796998b55eee0133ac8478a71e9c1ffeca6d
Park-gi-yeol <v2611317@naver.com>
2026-05-13 17:19:39 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.02 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
-		String[] arr = my_string.split("");
-		for(int i = my_string.length() - n; i < my_string.length(); i++) {
-			answer+=arr[i];
-		}
-        return answer;
+        return answer = my_string.substring(my_string.length() - n);
     }
 }
