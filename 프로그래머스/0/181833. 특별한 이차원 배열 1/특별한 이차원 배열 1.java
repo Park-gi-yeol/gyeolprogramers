6db4bd54749cbdebcdce4ad345a24cbb9d70b212
Park-gi-yeol <v2611317@naver.com>
2026-05-13 15:04:04 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 16.34 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
+		
 		for(int i = 0; i < answer.length; i++) {
-			for(int j =0; j < answer.length; j++) {
-				if(i == j) {
-					answer[i][j] = 1;
-				}
+			for(int j = 0; j < answer.length; j++) {
+				answer[i][j] = (i == j) ? 1 : 0;
+				System.out.print(answer[i][j]);
 			}
 		}
         return answer;
