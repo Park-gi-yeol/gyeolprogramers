f1476f407ea0d2659ed93b5b182b2993767fd014
Park-gi-yeol <v2611317@naver.com>
2026-05-13 14:28:01 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
 class Solution {
-    public ArrayList<Integer> solution(int start_num, int end_num) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-		for(int i = start_num; i <= end_num; i++) {
-			arr.add(i);
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+		for(int i = 0; i < answer.length; i++) {
+			answer[i] = start_num;
+			start_num++;
 		}
-        
-        return arr;
+        return answer;
     }
 }
