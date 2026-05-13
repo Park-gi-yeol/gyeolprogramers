f377679da7cc38302b7196bc1ab71424d04d4783
Park-gi-yeol <v2611317@naver.com>
2026-05-13 17:17:42 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
-        String[] arr = my_string.split("");
-		for(int i = 0; i < n; i++) {
-			answer += arr[i];
-		}
-        return answer;
+        return answer = my_string.substring(0,n);
     }
 }
